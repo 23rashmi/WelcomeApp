e2748da6817f2a7b9cf9dae7f4ce3fe656e597ff
Rashmi Priya <priye98priya@gmail.com>
2024-02-15 12:03:16 +0530

    initial commit

---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "UserName and Password required";
+            }
+            else
+            {
+                if((userName == "admin") && (password == "admin@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Failed";
+                }
+            }
+            return msg;
+        }
+    }
+}
